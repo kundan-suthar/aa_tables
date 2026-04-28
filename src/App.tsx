083f8bc3050842b1c7568292c64342e2745369cd
kundan suthar <kundanss22@gmail.com>
2026-04-28 17:36:29 +0530

    add sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import Layout from "./layouts/layout"
 
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Layout>
+      <h1>hello</h1>
+    </Layout>
   )
 }
 
